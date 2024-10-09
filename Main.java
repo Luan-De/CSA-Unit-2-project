086fbbe5c03aa264a9ed1ecd6adc14c372094dab
Eclipse-V <christinevanson77@gmail.com>
2024-10-09 14:04:11 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("If you provide me some information I will provide a Science Fiction name for you");
+        System.out.println("Please have all responses be at least three characters longs.");
+        System.out.println("Enter your first name: ");
+        String firstName = input.nextLine();
+        System.out.println("Enter your last name: ");
+        String lastName = input.nextLine();
+        String name = firstname.substring(0, 3) + lastName.substring(0, 2).toLowerCase();
+    }
+}
