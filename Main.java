cbce5519776430345dc93e929b68b26ee6a47025
Eclipse-V <christinevanson77@gmail.com>
2024-10-09 14:29:50 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
+//Name: Eclipse Vanson and Luan Dematos
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        //String Inputs
         System.out.println("If you provide me some information I will provide a Science Fiction name for you");
         System.out.println("Please have all responses be at least three characters longs.");
         System.out.println("Enter your first name: ");
@@ -17,9 +19,18 @@ public class Main{
         System.out.println("Enter the first name of a second sibling or relative: ");
         String familyRelative2 = input.nextLine();
 
+        //Random Number generator
+        int num = (int)(Math.random() * ((familyRelative1.length() - 1) - 1)) + 1;
+        int num2 = (int)(Math.random() * ((familyRelative2.length() - 1) - 1)) + 1;
 
 
+        //Sci Fi varibles 
+        String sciFiFirstname = firstName.substring(0, 3) + lastName.substring(0, 2).toLowerCase();
+        String sciFiLastname = city.substring(0,2) + grammerSchool.substring(-3).toLowerCase();
+        String placeOfOri = familyRelative1.substring(num) + familyRelative2.substring(num2);
+
+        //Welcome message
+        System.out.println("Welcome " + sciFiFirstname + " " + sciFiLastname + " " + placeOfOri + "!");
         
-        String SciFiFirstname = firstName.substring(0, 3) + lastName.substring(0, 2).toLowerCase();
     }
 }
