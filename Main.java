9a200cd86f59f24a88e9d6401222cb1d640bd223
Eclipse-V <christinevanson77@gmail.com>
2024-10-09 14:08:26 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,6 +8,18 @@ public class Main{
         String firstName = input.nextLine();
         System.out.println("Enter your last name: ");
         String lastName = input.nextLine();
+        System.out.println("Enter the city where you or one of your parents were born: ");
+        String city = input.nextLine();
+        System.out.println("Enter the name of your grammer school: ");
+        String grammerSchool = input.nextLine();
+        System.out.println("Enter the first name of a sibling or relative: ");
+        String familyRelative1 = input.nextLine();
+        System.out.println("Enter the first name of a second sibling or relative: ");
+        String familyRelative2 = input.nextLine();
+
+
+
+        
         String SciFiFirstname = firstname.substring(0, 3) + lastName.substring(0, 2).toLowerCase();
     }
 }
