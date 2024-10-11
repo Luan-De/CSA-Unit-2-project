3f914e6480f0f05d3a738036b51980441b1bc5eb
Eclipse-V <christinevanson77@gmail.com>
2024-10-11 10:44:14 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -26,15 +26,22 @@ public class Main{
         int num2 = (int)(Math.random() * ((familyRelative2.length() - 1) - 1)) + 1;
 
         //get the first letter in the string and capitalize it
+        String capital = firstName.substring(0,1).toUpperCase();
+        String restOfString = firstName.substring(1, 3);
+        String capFirstName = capital + restOfString;
+        
         String letter = familyRelative1.substring(num);
         String capitalFamilyRelative1 = letter.substring(0,1).toUpperCase();
         String familyrev = letter.substring(1);
         String firstNameOfRev = capitalFamilyRelative1 + familyrev;
         
+        String letter2 = city.substring(0, 1).toUpperCase();
+        String restOfString1 = city.substring(1, 2);
+        String capCity = letter2 + restOfString1;
         
         //Sci Fi variables 
-        String sciFiFirstname = firstName.substring(0, 3) + lastName.substring(0, 2).toLowerCase();
-        String sciFiLastname = city.substring(0, 2) + grammerSchool.substring(grammerSchool.length()-3).toLowerCase();
+        String sciFiFirstname = capFirstName + lastName.substring(0, 2).toLowerCase();
+        String sciFiLastname = capCity + grammerSchool.substring(grammerSchool.length()-3).toLowerCase();
         String placeOfOri = firstNameOfRev + familyRelative2.substring(num2);
 
         //Welcome message
