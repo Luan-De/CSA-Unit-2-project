9b4931e9e89f8e0fe2187db1b651295f2ae814d1
Eclipse-V <christinevanson77@gmail.com>
2024-10-11 10:16:16 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 //Name: Eclipse Vanson and Luan Dematos
+//Description: Sci-Fi name generator
+
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
@@ -23,14 +25,14 @@ public class Main{
         int num = (int)(Math.random() * ((familyRelative1.length() - 1) - 1)) + 1;
         int num2 = (int)(Math.random() * ((familyRelative2.length() - 1) - 1)) + 1;
 
-        //
+        //get the first letter in the string and capitalize it
         String letter = familyRelative1.substring(num);
         String capitalFamilyRelative1 = letter.substring(0,1).toUpperCase();
         
         
-        //Sci Fi varibles 
+        //Sci Fi variables 
         String sciFiFirstname = firstName.substring(0, 3) + lastName.substring(0, 2).toLowerCase();
-        String sciFiLastname = city.substring(0,2) + grammerSchool.substring(grammerSchool.length()-3).toLowerCase();
+        String sciFiLastname = city.substring(0, 2) + grammerSchool.substring(grammerSchool.length()-3).toLowerCase();
         String placeOfOri = capitalFamilyRelative1 + familyRelative2.substring(num2);
 
         //Welcome message
